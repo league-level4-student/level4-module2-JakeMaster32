03c50aaa9205c13e913b8e346c604f07351969f7
JakeMaster32 <46147844+JakeMaster32@users.noreply.github.com>
2021-02-08 18:30:35 -0800

    String methods

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -33,48 +33,73 @@ public class StringMethods {
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
 		String longer = "";
-		if(s1.length()>s2.length()) {
+		if (s1.length() >= s2.length()) {
 			longer = s1;
-		}
-		else if (s2.length()>s1.length()){
+		} else if (s2.length() > s1.length()) {
 			longer = s2;
 		}
-		
+
 		return longer;
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		if(s.contains("underscores")) {
-			s.replace(" ", "_");
+		if (s.contains("underscores")) {
+			s = s.replace(" ", "_");
 		}
 		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		
-		return null;
+		String[] order = null;
+		s1 = s1.trim();
+		s2 = s2.trim();
+		s3 = s3.trim();
+		String[] string1 = s1.split(" ");
+		String[] string2 = s2.split(" ");
+		String[] string3 = s3.split(" ");
+		if ((string1[1].compareTo(string2[1]) < 0) && string1[1].compareTo(string3[1]) < 0) {
+			order = string1;
+		} else if (string2[1].compareTo(string3[1]) < 0 && string2[1].compareTo(string1[1]) < 0) {
+			order = string2;
+		} else {
+			order = string3;
+		}
+
+		return order[0] + " " + order[1];
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int sum = 0;
+		for (int x = 0; x < s.length(); x++) {
+			if (Character.isDigit(s.charAt(x))) {
+				sum = sum + s.charAt(x);
+			}
+		}
+
+		return sum;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int counter = 0;
+		while (s.contains(substring)) {
+			counter++;
+			s = s.replaceFirst(substring, "");
+		}
+		
+		return counter;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
+
 		return null;
 	}
 
@@ -83,29 +108,46 @@ public class StringMethods {
 		return null;
 	}
 
-
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+		int number = 0;
+while (s.contains(substring+" ")) {
+	number++;
+	s = s.replaceFirst(substring+" ", "");
+}
+
+		return number;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+		int distance;
+		distance = s.lastIndexOf(substring) - (s.indexOf(substring)+substring.length());
+		return distance;
 	}
-
 
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
+		boolean b = false;
+		for (int x = 0; x< s.length()/2; x++) {
+			System.out.println(s.charAt(x));
+			System.out.println(s.charAt(s.length()-x-1));
+			System.out.println(b);
+			if (s.charAt(x) == s.charAt(s.length()-x)) {
+				b = true; 
+			}
+			else {
+				b = false;
+			}
+		}
 		return true;
 	}
-	
+
 }
 
 class Utilities {
